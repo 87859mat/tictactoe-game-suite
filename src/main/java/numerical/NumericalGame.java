d0310464d75d62ca981ac85ffe04d282e27dbd8d
ematiwos <ematiwos@uoguelph.ca>
2022-11-24 18:35:35 -0500

    added missing exception to takeTurn in numerical

--- src/main/java/numerical/NumericalGame.java
+++ src/main/java/numerical/NumericalGame.java
@@ -45,7 +45,7 @@ public class NumericalGame extends BoardGame implements Saveable{
         }
         
         if(!isValidMove(inputNum)) {
-            return false;
+            throw new RuntimeException("Invalid input");
         } else if(across < 0 || across > 2 || down < 0 || down > 2) {
             return false;
         } else if(!this.getGrid().getValue(across + 1, down + 1).equals(" ")) {
